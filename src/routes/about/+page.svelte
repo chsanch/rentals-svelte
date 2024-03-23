cbb4bd4a9157bf4a4e3599af60f092d34a079219
Christian Sánchez <chsanch@pm.me>
2024-03-23 12:54:37 +0100

    add route about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<section class="dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">About Super Rentals!</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">The Super Rentals website is a delightful project created to explore Ember.
+    By building a property rental site, we can simultaneously imagine traveling
+    AND building Ember applications.</p>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:w-96 lg:h-96">
+            <img src="images/teaching-tomster.png" alt="tomster">
+        </div>                
+    </div>
+</section>

a2dbb24111dbcab2073ecbaf8539667bc06845a6
Christian Sánchez <chsanch@pm.me>
2024-03-31 19:48:53 +0200

    add action to image component

--- src/lib/components/rental/image.svelte
+++ src/lib/components/rental/image.svelte
@@ -1,6 +1,28 @@
-<img
-	data-testid="rental-image"
-	class="w-48 md:w-64 md:h-auto mx-auto pr-4"
-	loading="lazy"
-	{...$$restProps}
-/>
+<script>
+	let dialogOpen = $state(false);
+	let { ...imgProps } = $props();
+	import * as Dialog from '$lib/components/ui/dialog';
+</script>
+
+<div class="text-center space-y-4 p-2">
+	<img
+		data-testid="rental-image"
+		class="w-48 md:w-64 md:h-auto mx-auto pr-4"
+		loading="lazy"
+		{...imgProps}
+	/>
+	<button on:click={() => (dialogOpen = true)} class="text-xs" data-testid="rental-image-button"
+		>View Larger</button
+	>
+</div>
+<Dialog.Root bind:open={dialogOpen}>
+	<Dialog.Content class="sm:max-w-[825px]">
+		<img
+			data-testid="rental-image-larger"
+			class="w-full h-auto mx-auto pr-4"
+			loading="lazy"
+			{...imgProps}
+		/>
+	</Dialog.Content>
+	<Dialog.Trigger />
+</Dialog.Root>
--- tests/unit/rental-image.test.js
+++ tests/unit/rental-image.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/svelte/svelte5';
-import { expect, test } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/svelte/svelte5';
+import { expect, test, vi } from 'vitest';
 import RentalImnage from '$lib/components/rental/image.svelte';
+import userEvent from '@testing-library/user-event';
 
 test('renders rental image component', () => {
   render(RentalImnage);
   expect(screen.getByTestId('rental-image')).toBeInTheDocument();
+  // remove component
+  cleanup(); 
+});
+
+test('renders rental image component with image', async () => {
+	const user = userEvent.setup();
+	const onClick = vi.fn();
+	const { component } = render(RentalImnage, { src: 'https://example.com/image.jpg' });
+
+	expect(screen.getByTestId('rental-image')).toBeInTheDocument();
+	expect(screen.getByTestId('rental-image')).toHaveAttribute(
+		'src',
+		'https://example.com/image.jpg'
+	);
+
+	component.$on('click', onClick);
+
+	const button = screen.getByTestId('rental-image-button');
+	await user.click(button);
+
+	expect(screen.getByTestId('rental-image-larger')).toBeInTheDocument();
+  cleanup(); 
 });

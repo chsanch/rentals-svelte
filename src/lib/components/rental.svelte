96e4c2021584200648559bfe2cdc927fc0c4ea96
Christian Sánchez <chsanch@pm.me>
2024-04-06 21:04:19 +0200

    fix rentals CSS

--- src/lib/components/rental.svelte
+++ src/lib/components/rental.svelte
@@ -17,30 +17,30 @@
 
 <Card.Root class="mb-4 md:mb-2">
 	<Card.Header>
-		<Card.Title data-testid="rental-title"><a href={url}>{title}</a></Card.Title>
+		<Card.Title data-testid="rental-title" class="text-3xl font-bold px-2 mb-0 text-center md:text-left"><a href={url}>{title}</a></Card.Title>
 	</Card.Header>
 	<Card.Content class="md:flex">
 		<RentalImage src={image} alt={`A picture of ${title}`} />
-		<div class="grid w-full items-center gap-4 md:grid-cols-2">
-			<div class="flex flex-col space-y-1.5" data-testid="rental-owner">
-				<span class="text-sm font-semibold text-gray-800">Owner:</span>
-				<span class="text-sm italic">{owner}</span>
+		<div class="grid w-full md:max-h-48 mt-0 md:grid-cols-2 md:grid-rows-4 md:ml-3">
+			<div class="flex flex-col mb-2" data-testid="rental-owner">
+				<span class="font-semibold text-gray-800">Owner:</span>
+				<span class="italic">{owner}</span>
 			</div>
-			<div class="flex flex-col space-y-1.5" data-testid="rental-location">
-				<span class="text-sm font-semibold text-gray-800">City:</span>
-				<span class="text-sm italic">{city}</span>
+			<div class="flex flex-col mb-2" data-testid="rental-location">
+				<span class="font-semibold text-gray-800">City:</span>
+				<span class="italic">{city}</span>
 			</div>
-			<div class="flex flex-col space-y-1.5" data-testid="rental-type">
-				<span class="text-sm font-semibold text-gray-800">Type:</span>
-				<span class="text-sm italic">{type} - {category}</span>
+			<div class="flex flex-col mb-2" data-testid="rental-type">
+				<span class="font-semibold text-gray-800">Type:</span>
+				<span class="italic">{type} - {category}</span>
 			</div>
-			<div class="flex flex-col space-y-1.5" data-testid="rental-bedrooms">
-				<span class="text-sm font-semibold text-gray-800">Bedrooms:</span>
-				<span class="text-sm italic">{bedrooms}</span>
+			<div class="flex flex-col mb-2" data-testid="rental-bedrooms">
+				<span class="font-semibold text-gray-800">Bedrooms:</span>
+				<span class="italic">{bedrooms}</span>
 			</div>
 		</div>
 		<div class="mt-1 mx-auto">
-			<Map lat={location.lat} lng={location.lng} zoom={10} width={354} height={246} />
+			<Map lat={location.lat} lng={location.lng} zoom={10} width={254} height={146} />
 		</div>
 	</Card.Content>
 </Card.Root>
--- src/lib/components/rental/image.svelte
+++ src/lib/components/rental/image.svelte
@@ -4,16 +4,20 @@
 	import * as Dialog from '$lib/components/ui/dialog';
 </script>
 
-<div class="text-center space-y-4 p-2">
+<div class="text-center md:space-y-4 md:p-2">
 	<img
 		data-testid="rental-image"
-		class="md:min-w-48 md:max-w-96 md:min-auto mx-auto pr-4"
+		class="md:min-w-48 md:max-w-64 md:min-auto mx-auto pr-4"
 		loading="lazy"
 		{...imgProps}
 	/>
-	<button on:click={() => (dialogOpen = true)} class="md:text-xs" data-testid="rental-image-button"
-		>View Larger</button
+	<button 
+		on:click={() => (dialogOpen = true)} 
+		class="invisible md:visible md:text-xs md:display-block" 
+		data-testid="rental-image-button"
 	>
+		View Larger
+	</button>
 </div>
 <Dialog.Root bind:open={dialogOpen}>
 	<Dialog.Content class="sm:max-w-[825px]">

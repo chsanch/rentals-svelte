f583297ac7c334bc0576a4ee99cfc4d052b139fc
Christian Sánchez <chsanch@pm.me>
2024-06-27 21:16:21 +0200

    change slot for @render children

--- src/lib/components/jumbo.svelte
+++ src/lib/components/jumbo.svelte
@@ -13,10 +13,14 @@
 		</p>
 	</Jumbo>
 -->
+<script>
+	/** @type {{ children: import('svelte').Snippet }}*/
+	let { children } = $props();
+</script>
 <section>
 	<div class="grid max-w-screen-xl px-4 py-4 mx-auto md:gap-8 xl:gap-x-36 lg:grid-cols-12">
 		<div class="mr-auto place-self-center lg:col-span-7" data-testid="jumbo-content">
-			<slot />
+			{@render children()}
 		</div>
 		<div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:w-80 lg:h-80" data-testid="jumbo-image">
 			<img src="/images/teaching-tomster.png" alt="tomster" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
   import NavBar from '$lib/components/nav-bar.svelte';
+  /** @type {{children: import('svelte').Snippet}}*/
+  let { children } = $props();
 </script>
 <NavBar />
-<slot />
+{@render children()}
